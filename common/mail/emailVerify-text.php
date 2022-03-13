5f4686c9ef12836a534209ccb1c69d8ca68dcb0e
artmart <artrmart@gmail.com>
2022-03-13 11:48:09 +0400

    clean

--- common/mail/emailVerify-text.php
+++
@@ -1,8 +0,0 @@
-<?php
-$verifyLink = Yii::$app->urlManager->createAbsoluteUrl(['site/verify-email', 'token' => $user->verification_token]);
-?>
-Hello <?= $user->firstname ?>,
-
-Follow the link below to verify your email:
-
-<?= $verifyLink ?>
